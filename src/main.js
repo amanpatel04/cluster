f00496c572aa8e9bcccbe53571b76930c9dc911a
aman219 <65351629+aman219@users.noreply.github.com>
2024-12-27 16:17:18 +0530

    A small error message fixed in main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 
-const {app} = require("./app");
+const { app } = require("./app");
 const { dbConnect } = require("./db/connect");
 
 dbConnect()
-.then(() => {
-    app.listen(process.env.PORT, () => {
-        console.log(`Server running on port ${process.env.PORT}`);
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Server running on port ${process.env.PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(`Database connection error : ${error}`);
+        process.exit(1);
     });
-})
-.catch((error) => {
-    console.log(`Database schema creation error : ${error}`);
-    process.exit(1);
-});
